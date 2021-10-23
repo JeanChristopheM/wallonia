8ac2da9dcbb31e56396aeb611bc4727eba4eb8da
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-10-23 14:54:18 +0200

    finished layers

--- src/script/main.js
+++ src/script/main.js
@@ -5,6 +5,7 @@ const app = async () => {
     const tiles = L.tileLayer(tileUrl, { attribution });
     tiles.addTo(mymap);
     
+    //MapIcon class
     const mapIcon = L.Icon.extend({
         options: {
             iconSize:     [20, 20],
@@ -14,15 +15,25 @@ const app = async () => {
             popupAnchor:  [-3, -76]
         }
     });
+    //Customizing icons and setting up layers
     const artIcon = new mapIcon({iconUrl: './media/mapIcons/color-palette-10818.svg', shadowUrl: './media/mapIcons/empty.png'});
+    let artLayer = L.layerGroup();
     const tourismIcon = new mapIcon({iconUrl: './media/mapIcons/map-signs-solid.svg', shadowUrl: './media/mapIcons/empty.png'});
+    let tourismLayer = L.layerGroup();
     const cultureIcon = new mapIcon({iconUrl: './media/mapIcons/theater-masks-solid.svg', shadowUrl: './media/mapIcons/empty.png'});
+    let cultureLayer = L.layerGroup();
     const atmIcon = new mapIcon({iconUrl: './media/mapIcons/euro-sign-solid.svg', shadowUrl: './media/mapIcons/empty.png'});
+    let atmLayer = L.layerGroup();
     const playgroundIcon = new mapIcon({iconUrl: './media/mapIcons/child-solid.svg', shadowUrl: './media/mapIcons/empty.png'});
+    let playgroundLayer = L.layerGroup();
     const horecaIcon = new mapIcon({iconUrl: './media/mapIcons/utensils-solid.svg', shadowUrl: './media/mapIcons/empty.png'});
+    let horecaLayer = L.layerGroup();
     const commerceIcon = new mapIcon({iconUrl: './media/mapIcons/shopping-cart-solid.svg', shadowUrl: './media/mapIcons/empty.png'});
+    let commerceLayer = L.layerGroup();
     const sportIcon = new mapIcon({iconUrl: './media/mapIcons/futbol-solid.svg', shadowUrl: './media/mapIcons/empty.png'});
+    let sportLayer = L.layerGroup();
     const commuIcon = new mapIcon({iconUrl: './media/mapIcons/people-carry-solid.svg', shadowUrl: './media/mapIcons/empty.png'});
+    let commuLayer = L.layerGroup();
     /*
     const dataSources = {
         art: 'https://www.odwb.be/api/records/1.0/search/?dataset=art-dans-la-ville&q=&facet=nomenclature_de_pois',
@@ -51,8 +62,12 @@ const app = async () => {
         const response = await data.json();
         return response.records;
     }
-    const addMarker = (coords, icon, popup) => {
-        L.marker(coords, {icon: icon}).addTo(mymap).bindPopup(popup);
+    const addMarker = (coords, icon, popup, layer) => {
+        if (layer) {
+            L.marker(coords, {icon: icon}).addTo(layer).bindPopup(popup);
+        } else {
+            L.marker(coords, {icon: icon}).addTo(mymap).bindPopup(popup);
+        }
     }
     const dataSets = [];
     for (x=0;x<dataUrls.length;x++) {
@@ -61,53 +76,73 @@ const app = async () => {
     }
     console.log(dataSets);
     for (let item of dataSets) {
-
         for (let i of item) {
             let icon;
             let popup;
+            let layer;
             switch (i.datasetid) {
                 case 'art-dans-la-ville':
                     icon = artIcon;
                     popup = `${i.fields.nomenclature_de_pois} : ${i.fields.titre}`;
+                    layer = artLayer;
                     break;
                 case 'tourisme':
                     icon = tourismIcon;
                     popup = `${i.fields.nomenclature_de_pois} : ${i.fields.titre}`;
+                    layer = tourismLayer;
                     break;
                 case 'antoing-culture':
                     icon = cultureIcon;
                     popup = `${i.fields.titre}`;
+                    layer = cultureLayer;
                     break;
                 case 'antoing-distributeurs-dargent':
                     icon = atmIcon;
                     popup = `Distrubuteur ${i.fields.titre}`;
+                    layer = atmLayer;
                     break;
                 case 'aires-de-jeux0':
                     icon = playgroundIcon;
                     popup = `Plaine de jeux ${i.fields.titre}`;
+                    layer = playgroundLayer;
                     break;
                 case 'horeca':
                     icon = horecaIcon;
                     popup = `${i.fields.nomenclature_de_pois} : ${i.fields.titre}`;
+                    layer = horecaLayer;
                     break;
                 case 'antoing-commerces':
                     icon = commerceIcon;
                     popup = `${i.fields.nomenclature_de_pois} : ${i.fields.titre}`;
+                    layer = commerceLayer;
                     break;
                 case 'antoing-sports':
                     icon = sportIcon;
                     popup = `${i.fields.nomenclature_de_pois} : ${i.fields.titre}`;
+                    layer = sportLayer;
                     break;
                 case 'vie-associative':
                     icon = commuIcon;
                     popup = `${i.fields.titre}`;
+                    layer = commuLayer;
                     break;
                 default:
                     console.log('error');
             }
-            addMarker(i.fields.coordonnees, icon, popup);
+            addMarker(i.fields.coordonnees, icon, popup, layer);
         }
     }
-    
+    const overlays = {
+        "Art": artLayer,
+        "Tourism": tourismLayer,
+        "Culture": cultureLayer,
+        "ATM": atmLayer,
+        "Playgrounds": playgroundLayer,
+        "Horeca": horecaLayer,
+        "Shops": commerceLayer,
+        "Sports": sportLayer,
+        "Communities": commuLayer
+    }
+    L.control.layers(null, overlays).addTo(mymap);
 }
 app();
